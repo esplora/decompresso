de7e478c041a551561f2e99f746a36c7b5c2739b
tabuna <tabuna@users.noreply.github.com>
2024-11-13 21:20:19 +0000

    Fix code styling

--- src/Adapters/MSOfficeCryptoToolAdapter.php
+++ src/Adapters/MSOfficeCryptoToolAdapter.php
@@ -79,7 +79,7 @@ class MSOfficeCryptoToolAdapter implements AdapterInterface
 
         // Add password option if provided
         if ($password !== null) {
-            $command[] = '--password=' . $password;
+            $command[] = '--password='.$password;
         } else {
             $command[] = '--test';
         }
